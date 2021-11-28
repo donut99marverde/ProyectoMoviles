ab148c29620c71aa4b9210f1de8d7ae0af80387c
pedro12346 <A01410693@itesm.mx>
2021-11-28 15:18:59 -0600

    Refactorización

--- app/src/main/java/com/example/proyecto/Pantalla_Hábito.kt
+++ app/src/main/java/com/example/proyecto/Pantalla_Hábito.kt
@@ -1,31 +1,39 @@
 package com.example.proyecto
 
+import android.content.Intent
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import org.w3c.dom.Text
 
-class Pantalla_Raciones_Frutas_Vegetales : AppCompatActivity() {
+class Pantalla_Hábito : AppCompatActivity() {
 
     private lateinit var habitManager: HabitManager
-
+    private lateinit var category : String
     var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pantalla_raciones_frutas_verduras)
-        title = "Raciones de frutas y vegetales"
-        habitManager = HabitManager(this)
-        val habit = habitManager.getHabit(getString(R.string.Raciones_de_frutas_y_vegetales))
-
+        setContentView(R.layout.activity_pantalla_habito)
+        val intent = intent
         val addButton = findViewById<Button>(R.id.addButton)
         val deleteButton = findViewById<Button>(R.id.deleteButton)
+        val timesPerDayTextView = findViewById<TextView>(R.id.timesPerDayTextView)
+        val completedLabel = findViewById<TextView>(R.id.completedLabel)
+        val leftLabel = findViewById<TextView>(R.id.leftLabel)
         val addCounter = findViewById<Button>(R.id.sumar)
         val subsCounter = findViewById<Button>(R.id.restar)
-        val counterTextView = findViewById<TextView>(R.id.counterTextView)
-        val leftTextView = findViewById<TextView>(R.id.leftTextView)
+
+        category = intent.getStringExtra("category")!!
+        title = category
+        timesPerDayTextView.text = intent.getStringExtra("timePerDayTextView")!!
+        completedLabel.text = intent.getStringExtra("completedLabel")
+        leftLabel.text = intent.getStringExtra("leftLabel")
+
+        habitManager = HabitManager(this)
 
         updateViews()
 
@@ -38,41 +46,32 @@ class Pantalla_Raciones_Frutas_Vegetales : AppCompatActivity() {
         }
 
         addCounter.setOnClickListener {
-            if(habitManager.isHabitActive(getString(R.string.Raciones_de_frutas_y_vegetales))) {
-                counter  += 1
-                counterTextView.text = counter.toString()
-                updateCounterData()
-            }
+            incrementCounter()
         }
 
         subsCounter.setOnClickListener {
-            if(habitManager.isHabitActive(getString(R.string.Raciones_de_frutas_y_vegetales))) {
-                if(counter <= 0){
-                    Toast.makeText(this, "Las raciones consumidas deben ser igual o mayor a cero", Toast.LENGTH_SHORT).show()
-                }
-                else{
-                    counter -= 1
-                    counterTextView.text = counter.toString()
-                }
-                updateCounterData()
-            }
+            substractCounter()
         }
     }
 
-
-    private fun updateCounterData() {
-        val completedTextView = findViewById<TextView>(R.id.completedTextView)
-        val leftTextView = findViewById<TextView>(R.id.leftTextView)
-        habitManager.setCompleted(getString(R.string.Raciones_de_frutas_y_vegetales), counter);
-        val habit = habitManager.getHabit(getString(R.string.Raciones_de_frutas_y_vegetales))
-
-        if((habit.timesPerDay - counter) <= 0){
-            leftTextView.text = "0"
+    private fun incrementCounter() {
+        val counterTextView = findViewById<TextView>(R.id.counterTextView)
+        if(habitManager.isHabitActive(category)) {
+            counter  += 1
+            counterTextView.text = counter.toString()
+            //actualizar en DB
+            habitManager.setCompleted(category, counter)
+            updateViews()
         }
-        else{
-            leftTextView.text = (habit.timesPerDay - counter).toString()
+    }
+
+    private fun substractCounter() {
+        if(habitManager.isHabitActive(category) && counter > 0) {
+            counter -= 1
+            //actualizar en DB
+            habitManager.setCompleted(category, counter)
+            updateViews()
         }
-        completedTextView.text = counter.toString()
     }
 
     private fun updateViews() {
@@ -83,45 +82,51 @@ class Pantalla_Raciones_Frutas_Vegetales : AppCompatActivity() {
         val completedTextView = findViewById<TextView>(R.id.completedTextView)
         val leftTextView = findViewById<TextView>(R.id.leftTextView)
         val counterTextView = findViewById<TextView>(R.id.counterTextView)
-        addButton.setBackgroundColor(Color.parseColor("#2196f3"))
-        deleteButton.setBackgroundColor(Color.parseColor("#c63f17"))
-        statusTextView.setPadding(20, 20, 20, 20)
-        val habit = habitManager.getHabit(getString(R.string.Raciones_de_frutas_y_vegetales))
 
-        habitManager.printHabitObj(habit)
+        addButton.setBackgroundColor(Color.parseColor("#2196f3"))
+        statusTextView.setPadding(20, 20, 20, 20)
+
+        val habit = habitManager.getHabit(category)
 
         if(habit.isActive == 1) {
-            var status = "El habito se encuentra activo"
+            var status = "El habito se encuentra activo "
             addButton.text = "Actualizar"
             deleteButton.isEnabled = true
             timesPerDayTextNumber.text = habit.timesPerDay.toString()
 
-            if(habit.alertTimes != null) {
-                status += " en los siguientes horarios: "
-
-                for(time in habit.alertTimes!!) {
-                    status += time + " "
-                }
+            if(habit.frequency == "daily") {
+                status += "todos los dias"
             } else {
-                status += " sin un horario en específico"
+                status += "los siguientes dias"
+                for(weekday in habit.daysOfTheWeek) {
+                    status += weekday
+                }
             }
 
             statusTextView.text = status
             statusTextView.setBackgroundColor(Color.parseColor("#c8e6c9"))
+            deleteButton.setBackgroundColor(Color.parseColor("#c63f17"))
             completedTextView.text = habit.completed.toString()
 
         } else {
             statusTextView.text = "El hábito no esta activo"
             statusTextView.setBackgroundColor(Color.parseColor("#ff867c"))
+            deleteButton.setBackgroundColor(Color.parseColor("#ef9a9a"))
             addButton.text = "Agregar"
             deleteButton.isEnabled = false
             timesPerDayTextNumber.text = "0"
             completedTextView.text = "0"
         }
 
+        /*Actualizar completados, restantes y contador*/
         counter = habit.completed
         completedTextView.text = counter.toString()
-        leftTextView.text = (habit.timesPerDay - habit.completed).toString()
+
+        if((habit.timesPerDay - counter) <= 0){
+            leftTextView.text = "0"
+        } else {
+            leftTextView.text = (habit.timesPerDay - habit.completed).toString()
+        }
         counterTextView.text = counter.toString()
     }
 
@@ -129,34 +134,31 @@ class Pantalla_Raciones_Frutas_Vegetales : AppCompatActivity() {
     private fun addHabit() {
         val addButton = findViewById<Button>(R.id.addButton)
         val timesPerDayTextNumber = findViewById<TextView>(R.id.timesPerDayTextNumber)
-        var timesPerDay = 0
-        val frequency = "daily"
-        var alertTimes = ArrayList<String>()
-        var daysOfTheWeek = ArrayList<String>()
         val counterTextView = findViewById<TextView>(R.id.counterTextView)
-        alertTimes.add("11:00")
-        daysOfTheWeek.add("monday")
-        daysOfTheWeek.add("wednesday")
+        var timesPerDay : Int
+        val frequency = "daily"
+        var daysOfTheWeek = ArrayList<String>()
+        daysOfTheWeek.add("Lunes")
+        daysOfTheWeek.add("Martes")
 
-        if(timesPerDayTextNumber.text.toString().length > 0) {
+        if(timesPerDayTextNumber.text.toString().isNotEmpty()) {
             timesPerDay = timesPerDayTextNumber.text.toString().toInt()
-
         } else {
             Toast.makeText(this, "El número de hábitos se encuentra vacio", Toast.LENGTH_SHORT).show()
             return
         }
 
-        var success = false
+        var success : Boolean
 
         if(frequency == "daily") {
-            success = habitManager.addDailyHabit(getString(R.string.Raciones_de_frutas_y_vegetales), timesPerDay, alertTimes)
+            success = habitManager.addDailyHabit(category, timesPerDay)
 
             if(!success) {
                 Toast.makeText(this, "Ocurrió un error al agregar el hábito diario", Toast.LENGTH_SHORT).show()
                 return
             }
         } else {
-            success = habitManager.addWeeklyHabit(getString(R.string.Raciones_de_frutas_y_vegetales), timesPerDay, alertTimes, daysOfTheWeek)
+            success = habitManager.addWeeklyHabit(category, timesPerDay, daysOfTheWeek)
 
             if(!success) {
                 Toast.makeText(this, "Ocurrió un error al agregar el hábito semanal", Toast.LENGTH_SHORT).show()
@@ -165,6 +167,7 @@ class Pantalla_Raciones_Frutas_Vegetales : AppCompatActivity() {
         }
 
         counterTextView.text = "0"
+
         if(addButton.text == "Actualizar") {
             Toast.makeText(this, "Se ha actualizado el hábito", Toast.LENGTH_SHORT).show()
         } else {
@@ -174,7 +177,7 @@ class Pantalla_Raciones_Frutas_Vegetales : AppCompatActivity() {
     }
 
     private fun deleteHabit() {
-        val success = habitManager.deleteHabit(getString(R.string.Raciones_de_frutas_y_vegetales))
+        val success = habitManager.deleteHabit(category)
 
         if(success) {
             Toast.makeText(this, "Hábito eliminado", Toast.LENGTH_SHORT).show()
@@ -183,5 +186,4 @@ class Pantalla_Raciones_Frutas_Vegetales : AppCompatActivity() {
         }
         updateViews()
     }
-
 }
